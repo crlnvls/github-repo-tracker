431deeea820ed554c5d040a366729075e6a5762d
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-31 11:34:08 +0100

    Add search form structure

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,1 +1,40 @@
-import React from 'react'
+import React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import SearchIcon from "@mui/icons-material/Search";
+import IconButton from "@mui/material/IconButton";
+
+export default function SearchForm({ getData }) {
+  const [user, setUser] = useState("");
+
+  function handleUser(e) {
+    const newUser = e.target.value;
+    setUser(newUser);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    e.target.reset();
+    getData(user);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+          <TextField
+            autoFocus
+            label="Search github user..."
+            variant="standard"
+            size="small"
+            onChange={handleUser}
+          />
+          <IconButton type="submit">
+            <SearchIcon style={{ fill: "white" }} />
+          </IconButton>
+        </Box>
+        ;
+      </form>
+    </>
+  );
+}
