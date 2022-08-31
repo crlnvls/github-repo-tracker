93165412e094256ffc478602ec4af895133d04df
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-31 17:06:15 +0100

    Add initial structure for cards

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Card = ({data}) => {
-    return <>
-    <div className="card" style={{width: '18rem'}}>
-  {/* <img src="..." className="card-img-top" alt="..."/> */}
-  <div className="card-body">
-    <div className="card-title">{data.name}</div>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href={data.clone_url} target='blank' className="btn btn-primary">{data.clone_url}</a>
-  </div>
-</div>
+const Card = ({ data }) => {
+  return (
+    <>
+      <div className="card" style={{ width: "40rem" }}>
+        <div className="card-body">
+          <div class="d-flex flex-row mb-2">
+            <a href={data.clone_url} target="blank">
+              {data.name}
+            </a>
+            <p className="mx-4  btn btn-outline-dark">{data["visibility"]}</p>
+          </div>
+          <p className="card-text">{data["description"]}</p>
+        </div>
+        <div class="d-flex flex-row mb-3">
+          <div class="p-2">Flex item 1</div>
+          <div class="p-2">Flex item 2</div>
+          <div class="p-2">Flex item 3</div>
+        </div>
+      </div>
     </>
-}
+  );
+};
 
 export default Card;
