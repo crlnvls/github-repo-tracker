64b3856fb63ae9d640d67bfae1bac3208ede1d50
azotamiota <azotamiota@gmail.com>
2022-08-31 15:28:58 +0100

    all APIs working

--- src/pages/User/index.jsx
+++ src/pages/User/index.jsx
@@ -1,34 +1,44 @@
 import React, {useState} from "react";
 import axios from "axios";
 
+import './style.css';
+
 import SearchForm from '../../components/SearchForm'
 
 const User = () => {
 
     const [user, setUser] = useState("");
-    const [repo, setRepo] = useState("");
+    const [repos, setRepo] = useState("");
 
     
     const getUser = async (incomingUsername) => {
 
         const userResult = await axios.request(`https://api.github.com/users/${incomingUsername}`)
-        console.log('user: ', userResult)
+        console.log('user: ', userResult.data)
         setUser(userResult)
     }
 
     const getRepo = async (incomingUsername) => {
         // setUser(banana)
-      const repos = await axios.request(`https://api.github.com/users/${incomingUsername}/repos`)
-      console.log('repo: ', repos)
-      setRepo(repos)
+      const reposResult = await axios.request(`https://api.github.com/users/${incomingUsername}/repos`)
+      console.log('repo: ', reposResult)
+      setRepo(reposResult)
     }
 
 
 
     return <>
+
     <h1>User</h1>
-    <SearchForm getUser={getUser} getRepo={getRepo}/>
-    <p>User: {user} Repo: {repo}</p>
+        <SearchForm getUser={getUser} getRepo={getRepo}/>
+    <div className='result' style={{display: !user ? 'none' : 'flex'}}>
+        <img src={user && user.data['avatar_url']} alt='image'/>
+        <p>Name: {user && user.data.name}</p>
+        <p>Repos names: {
+                repos && repos.data.map(repo => repo.name)
+            }</p>
+        {/* <p>User: {user} Repo: {repo}</p> */}
+    </div>
     </>
 }
 
