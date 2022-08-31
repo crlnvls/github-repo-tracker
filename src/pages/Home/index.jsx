6601c8c760772cc14984ee8fab5d7693f80d6c5e
azotamiota <azotamiota@gmail.com>
2022-08-31 16:33:46 +0100

    add Home page style

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
+import {Link} from 'react-router-dom' 
+import './style.css'
+
 const Home = () => {
     return <>
-    <h1>Home</h1>
+    <div className="hero">
+        <h1>Welcome to GitHub API!</h1>
+        <h3>Easy access to millions of GitHub repositories.</h3><br></br>
+        <p>Let's get started by <Link to='user'>searching users</Link></p>
+    </div>
     </>
 }
 
