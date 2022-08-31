7ee526003beea46390005fc2d08e62cca4f7fef3
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-31 13:48:18 +0100

    Update serachForm

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import SearchIcon from "@mui/icons-material/Search";
@@ -7,17 +7,16 @@ import IconButton from "@mui/material/IconButton";
 export default function SearchForm({ getData }) {
   const [user, setUser] = useState("");
 
-  function handleUser(e) {
-    const newUser = e.target.value;
-    setUser(newUser);
-  }
-
   function handleSubmit(e) {
     e.preventDefault();
     e.target.reset();
     getData(user);
   }
 
+  function handleUser(e) {
+    const input = e.target.value;
+    setUser(input);
+  }
   return (
     <>
       <form onSubmit={handleSubmit}>
@@ -30,7 +29,7 @@ export default function SearchForm({ getData }) {
             onChange={handleUser}
           />
           <IconButton type="submit">
-            <SearchIcon style={{ fill: "white" }} />
+            <SearchIcon style={{ fill: "black" }} />
           </IconButton>
         </Box>
         ;
