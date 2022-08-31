b7ad0ad8f6dd5ff4420b4119904ed6f49ab90d2f
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-31 14:02:52 +0100

    Add new navBar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,34 +1,38 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import SearchForm from '../SearchForm';
-
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-    return <>
-    <nav className="navbar navbar-expand-lg bg-dark">
-  <div className="container-fluid">
-    <Link className="navbar-brand" to="#">Navbar</Link>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <Link className="nav-link active" aria-current="page" to="#">Home</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="#">Link</Link>
-        </li>
-      </ul>
-      <SearchForm/>
-      {/* <form className="d-flex" role="search">
-        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button className="btn btn-outline-success" type="submit">Search</button>
-      </form> */}
-    </div>
-  </div>
-</nav>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg bg-dark">
+        <div className="container-fluid">
+          <img
+            src="https://www.freeiconspng.com/thumbs/github-icon/github-icon-9.png"
+            alt="github icon"
+            width="60px"
+          />
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active text-white"
+                  aria-current="page"
+                  to="/"
+                >
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item ">
+                <Link className="nav-link text-white" to="/user">
+                  User
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </>
-}
+  );
+};
 
 export default Navbar;
