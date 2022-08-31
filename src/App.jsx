c83c4333af9f95fccaa6865073f9f2f4c01cfabc
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-31 11:46:55 +0100

    Updated the routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import * as Pages from "./pages";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Pages.Home />}>
-        <Route path="/:repos" element={<Pages.Repo />}></Route>
-      </Route>
+      <Route path="/" element={<Pages.Home />}></Route>
+      <Route path="/:user" element={<Pages.Home />}></Route>
+      <Route path="/:user/:repo" element={<Pages.Repo />}></Route>
       <Route path="*" element={<Pages.NothingFound />}></Route>
     </Routes>
   );
