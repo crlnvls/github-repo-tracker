4e7ed1d894ffe658c229439e1ec7c67b3ab84805
“crlnvls” <cveloso@hotmail.co.uk>
2022-08-31 11:55:38 +0100

    Add user page and user route

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Pages.Home />}></Route>
-      <Route path="/:user" element={<Pages.Home />}></Route>
+      <Route path="/:user" element={<Pages.User />}></Route>
       <Route path="/:user/:repo" element={<Pages.Repo />}></Route>
       <Route path="*" element={<Pages.NothingFound />}></Route>
     </Routes>
---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as NothingFound } from "./NothingFound";
+export { default as Repo } from "./Repo";
+export { default as User } from "./User";
